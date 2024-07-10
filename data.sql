c17beb9783d393a3039797a0f234e1df3fe3f3ad
rahulgupta1432 <rahulkumar058790@gmail.com>
2024-07-11 04:00:32 +0530

    chore: Create referral table in data.sql
    The code changes in `data.sql` create a new table called `referral` with columns `id`, `referrer`, `referee`, and `createdAt`. This table will be used to store referral data.
    
    This commit message follows the established convention of using a prefix to indicate the type of change (`chore` for miscellaneous changes) and provides a clear and concise description of the changes made.

---
+++ data.sql
@@ -0,0 +1,13 @@
+/*
+SQLyog Community v13.2.1 (64 bit)
+MySQL - 8.0.37 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `referral` (
+	`id` int (11),
+	`referrer` varchar (573),
+	`referee` varchar (573),
+	`createdAt` datetime 
+); 
